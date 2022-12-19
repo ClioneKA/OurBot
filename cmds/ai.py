6bd5245a72b44cb86e2fccf3a71d2207320195cd
chao.chang <chao.chang@mexc.com>
2022-12-19 14:32:49 +0800

    Add ai draw feature

---
+++ cmds/ai.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+import os
+import openai
+import io
+import aiohttp
+
+hosting = {}
+
+class AI(Cog_Extension):
+
+    @commands.command(name='draw', help='draw a picture.')
+    @commands.has_role("VIP")
+    async def draw(self, ctx, pr):
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        response = openai.Image.create(
+            prompt=pr,
+            n=1,
+            size="1024x1024"
+        )
+        image_url = response['data'][0]['url']
+        async with aiohttp.ClientSession() as session:
+            async with session.get(image_url) as resp:
+                if resp.status != 200:
+                    return await ctx.channel.send('Could not download file...')
+                data = io.BytesIO(await resp.read())
+                await ctx.channel.send(file=discord.File(data, 'cool_image.png'))
+
+async def setup(bot):
+    await bot.add_cog(AI(bot))
--- cmds/event.py
+++ cmds/event.py
@@ -9,22 +9,11 @@ welcomeChannel = os.getenv('WELCOME_CHANNEL')
 
 
 class Event(Cog_Extension):
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        channel = self.bot.get_channel(int(welcomeChannel))
-        await channel.send(f'{member} join!')
 
     @commands.Cog.listener()
     async def on_message(self, msg):
         if msg.content == '阿鵝' and msg.author != self.bot.user:
             await msg.channel.send('<:Goose:714794396096135248>')
-
-    # @commands.Cog.listener()
-    # async def on_command_error(self, ctx, error):
-    #     if hasattr(ctx.command, 'on_error'):
-    #         return
-    #     if isinstance(error, commands.errors.MissingRequiredArgument):
-    #         await ctx.send('Missing parameters!')
 
 
 async def setup(bot):
